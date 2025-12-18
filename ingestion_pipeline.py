6e2ff8f206d85178a122d65f1b283d4f42855fb6
Manthan Shekhawat <manthanshekhawat@Manthans-MacBook-Air.local>
2025-12-18 07:13:43 +0530

    created an ingestion pipeline

--- ingestion_pipeline.py
+++ ingestion_pipeline.py
@@ -4,7 +4,7 @@ from langchain_text_splitters import CharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
 from langchain_chroma import Chroma
 from dotenv import load_dotenv
-
+load_dotenv()
 ## load the files
 
 def load_documents(docs_path= "docs"):
@@ -30,9 +30,56 @@ def load_documents(docs_path= "docs"):
         print(f"  Content preview: {doc.page_content[:100]}...")
         print(f"  metadata: {doc.metadata}")
   return documents
+def split_documents(documents, chunk_size=1000, chunk_overlap=0):
+    """Split documents into smaller chunks with overlap"""
+    print("Splitting documents into chunks...")
+
+    text_splitter = CharacterTextSplitter(
+        chunk_size=chunk_size,
+        chunk_overlap=chunk_overlap
+    )
+
+    chunks = text_splitter.split_documents(documents)
+
+    if chunks:
+
+        for i, chunk in enumerate(chunks[:5]):
+            print(f"\n--- Chunk {i+1} ---")
+            print(f"Source: {chunk.metadata['source']}")
+            print(f"Length: {len(chunk.page_content)} characters")
+            print(f"Content:")
+            print(chunk.page_content)
+            print("-" * 50)
+
+        if len(chunks) > 5:
+            print(f"\n... and {len(chunks) - 5} more chunks")
+
+    return chunks
+
+def create_vector_store(chunks, persist_directory="db/chroma_db"):
+  """Create and persist ChromaDB vector store"""
+  print("Creating embeddings and storing in ChromaDB...")
+
+  embedding_model = OpenAIEmbeddings(model="text-embedding-3-small")
+
+  # Create ChromaDB vector store
+  print("--- Creating vector store ---")
+  vectorstore = Chroma.from_documents(
+      documents=chunks,
+      embedding=embedding_model,
+      persist_directory=persist_directory,
+      collection_metadata={"hnsw:space": "cosine"}
+  )
+  print("--- Finished creating vector store ---")
+
+  print(f"Vector store created and saved to {persist_directory}")
+  return vectorstore
 
 
 if __name__ =="__main__":
   # main()
   docs_path = "docs"
+  persistent_directory = "db/chroma_db"
   documents = load_documents(docs_path)
+  chunks = split_documents(documents)
+  vectorstore = create_vector_store(chunks, persistent_directory)
